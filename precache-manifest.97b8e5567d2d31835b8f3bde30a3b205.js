e8c9d609bf28b2ded8aef3b2aae0c256cdf7257f
KratosvsEzio <jadude19@gmail.com>
2021-04-04 14:14:43 +0500

    First build commit

---
+++ precache-manifest.97b8e5567d2d31835b8f3bde30a3b205.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7302f594e2189d2d5aa7c9183f557ab8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52ef3a14397cc0f72164",
+    "url": "/static/css/main.f7a71201.chunk.css"
+  },
+  {
+    "revision": "9a4af5c2dd14ab0eafb0",
+    "url": "/static/js/2.9d3d5e89.chunk.js"
+  },
+  {
+    "revision": "52ef3a14397cc0f72164",
+    "url": "/static/js/main.bee2e0fd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
